ab501373d07e02e17af3de500aa269ca19768dc5
ll9 <llwierer@web.de>
2019-03-01 21:50:06 +0100

    init

--- DataTableAdapterObjectBug/Form1.cs
+++ DataTableAdapterObjectBug/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,22 @@ namespace DataTableAdapterObjectBug
         public Form1()
         {
             InitializeComponent();
+
+            using (var connection = GetConnection())
+            using (var command = new SQLiteCommand("SELECT * FROM TESTTABLE", connection))
+            using (var adapter = new SQLiteDataAdapter(command))
+            {
+                var table = new DataTable();
+                adapter.Fill(table);
+                var annonymousObject = table.Columns.Cast<DataColumn>().Select(c => new { c.ColumnName, c.DataType });
+            }
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            var connection = new SQLiteConnection("Data Source=Db\\db.sqlite");
+            connection.Open();
+            return connection;
         }
     }
 }
